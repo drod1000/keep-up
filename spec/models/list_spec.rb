54a8a30764b4c92ba8d97a93f7415c7dccb39c3c
Daniel Rodriguez <drod2793@gmail.com>
2017-02-21 17:53:37 -0700

    Adds test for list model

---
+++ spec/models/list_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe List, type: :model do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        list = List.new
+
+        expect(list).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a name" do
+        user = List.new(name: "Daniel")
+
+        expect(list).to be_valid
+      end
+    end
+  end
+end
