7ee382cfc944021dd47b531e07b1c5291a1c482c
Daniel Rodriguez <drod2793@gmail.com>
2017-02-21 18:24:01 -0700

    Adds test for article model

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a url" do
+        article = Article.new
+
+        expect(article).to be_invalid
+      end
+
+      it "has a unique url" do
+        url = "www.something.com/something"
+        article_1 = Article.create(url: url)
+        article_2 = Article.new(url: url)
+
+        expect(article_2).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with only a url" do
+        url = "www.something.com/something"
+        article = Article.new(url: url)
+
+        expect(article).to be_valid
+      end
+    end
+  end
+end
