334d40db71a818153bdcbd973c666823b00fb0a3
Daniel Rodriguez <drod2793@gmail.com>
2017-02-21 18:25:57 -0700

    Adds article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222010412) do
+ActiveRecord::Schema.define(version: 20170222012422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "url"
+    t.string   "title"
+    t.string   "author"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
