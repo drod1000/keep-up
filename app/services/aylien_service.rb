91215e7b839c8734bf27385c03186b44745e2591
Daniel Rodriguez <drod2793@gmail.com>
2017-02-22 09:08:41 -0700

    Fixes aylien service app key

--- app/services/aylien_service.rb
+++ app/services/aylien_service.rb
@@ -2,7 +2,7 @@ require 'aylien_text_api'
 
 class AylienService
   def self.extract(url)
-    client = AylienTextApi::Client.new(app_id: ENV["AYLIEN_APP_ID"], app_key: ENV["AYLIEN_API_KEY"])
+    client = AylienTextApi::Client.new(app_id: ENV["AYLIEN_APP_ID"], app_key: ENV["AYLIEN_APP_KEY"])
     client.extract(url: url)
   end
 end
