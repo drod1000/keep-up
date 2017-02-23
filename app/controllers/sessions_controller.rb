f4afcb39960e43447f565e5271b39f1d0a7c4d65
Daniel Rodriguez <drod2793@gmail.com>
2017-02-22 17:14:31 -0700

    Adds logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,12 @@ class SessionsController < ApplicationController
     redirect_to user_lists_path(@user)
   end
 
+  def destroy
+    reset_session
+    flash[:success] = "Successfully logged out!"
+    redirect_to root_path
+  end
+
   private
 
   def oauth_data
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #OAuth
   get '/auth/google_oauth2', as: :google_login
   get '/auth/google_oauth2/callback', to: 'sessions#create'
-
+  delete '/logout', to: 'sessions#destroy'
   resources :users, only: [:show] do
     resources :lists, only: [:index]
   end
