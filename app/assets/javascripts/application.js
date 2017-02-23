585518ef9a89b94b69027b728b28f3f2f22d98ba
Daniel Rodriguez <drod2793@gmail.com>
2017-02-23 15:23:14 -0700

    Adds JS to hit create list api endpoint

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,17 @@
 //= require jquery
 //= require bootstrap-sprockets
+
+$(document).ready(function(){
+
+  $('form').on('submit', function(){
+
+    var list = $('form input');
+    var listName = list.val();
+    var userId = $(this).attr('id')
+
+    $.ajax({
+      type: 'POST',
+      url: '/api/v1/lists?user_id=' + userId + '&list_name=' + listName
+    });
+  });
+});
