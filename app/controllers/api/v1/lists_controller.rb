9de564c7d2974360924764bf0a2f694409cd669e
Daniel Rodriguez <drod2793@gmail.com>
2017-02-24 10:32:12 -0700

    Removes extra methods

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,11 +1,4 @@
 class Api::V1::ListsController < Api::V1::BaseController
-  helper_method :current_user
-  add_flash_types :success, :info, :warning, :danger
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
   def create
     @list = current_user.lists.create(name: params[:list_name])
     render json: @list, :status => 201
