52dfe4c35d96764a1d4742599f34f90e61f5dd42
Daniel Rodriguez <drod2793@gmail.com>
2017-02-24 07:15:27 -0700

    Adds test for user creates an item

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'rspec/rails'
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock # or :fakeweb
+  config.allow_http_connections_when_no_cassette = true
 end
 # Add additional requires below this line. Rails is not loaded until this point!
 
