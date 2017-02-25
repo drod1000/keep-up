340ac26d9f2e9c61084ae4c01d2b70ee2c7e1440
Daniel Rodriguez <drod2793@gmail.com>
2017-02-24 17:25:30 -0700

    Creates S3 client

--- app/services/aws_service.rb
+++ app/services/aws_service.rb
@@ -2,8 +2,6 @@ require 'aws-sdk'
 
 class AwsService
   def self.text_to_speech(text)
-    Aws.config[:credentials] = Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
-
     client = Aws::Polly::Client.new(region: 'us-east-1')
     hash = {
       output_format: "mp3",
@@ -11,5 +9,10 @@ class AwsService
       voice_id: "Joanna"
     }
     client.synthesize_speech(hash)
+  end
+
+  def self.export_to_s3
+    client = Aws::S3::Client.new(
+    region: 'us-east-1')
   end
 end
