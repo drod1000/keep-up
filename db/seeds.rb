1a2c75bd64f9cb3883b6f754c7cdf69a0cad41ea
Daniel Rodriguez <drod2793@gmail.com>
2017-02-25 08:55:13 -0700

    Adds seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.start
+    seed = Seed.new
+    User.delete_all
+    List.delete_all
+    Article.delete_all
+    seed.generate_users_and_lists
+    seed.generate_articles
+    seed.articles_into_lists
+  end
+
+  def create_user
+    User.create!(
+      email:      Faker::Internet.email,
+      first_name: Faker::Name.first_name,
+      last_name:  Faker::Name.last_name
+    )
+  end
+
+  def generate_users_and_lists
+    user = create_user
+    5.times do
+      user.lists.create!(
+        name:     Faker::Lorem.word
+      )
+    end
+  end
+
+  def generate_articles
+    50.times do
+      Article.create!(
+        url:      Faker::Internet.url,
+        title:    Faker::Name.title,
+        author:   Faker::Book.author,
+        body:     Faker::Lorem.paragraph
+      )
+    end
+  end
+
+  def articles_into_lists
+    List.each do |list|
+      articles = Article.all.sample(10)
+      articles.each do |article|
+        list.articles << article
+      end
+    end
+  end
+end
+
+Seed.start
