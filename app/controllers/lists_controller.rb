22c335490b8609658c89cc54736fd575c559e7b9
Daniel Rodriguez <drod2793@gmail.com>
2017-03-01 12:18:38 -0700

    Adds test for unauthenticated user cannot view a users lists index page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
   def index
-    if current_user.id == params[:user_id].to_i
+    if current_user && params[:user_id].to_i == current_user.id
       render :index
     else
       render file: "/public/404"
--- spec/features/user_can_create_a_new_list_spec.rb
+++ spec/features/user_can_create_a_new_list_spec.rb
@@ -1,19 +1,30 @@
 require 'rails_helper'
 
-describe "When a user visits user_lists index page", js: true do
+describe "When a user visits user_lists index page"do
+  context "authenticated user" do
+    it "they can create a new list", js: true do
+      user = create(:user)
+      page.set_rack_session(user_id: user.id)
 
-  it "they can create a new list" do
-    user = create(:user)
-    page.set_rack_session(user_id: user.id)
+      visit user_lists_path(user)
 
-    visit user_lists_path(user)
+      fill_in 'list', :with => 'new list'
+      click_on("Add List")
 
-    fill_in 'list', :with => 'new list'
-    click_on("Add List")
+      sleep 2
 
-    sleep 2
+      expect(current_path).to eq(user_lists_path(user))
+      expect(page).to have_content('new list')
+    end
+  end
 
-    expect(current_path).to eq(user_lists_path(user))
-    expect(page).to have_content('new list')
+  context "unauthenticated user" do
+    it "they do not have access to this page" do
+      user = create(:user)
+
+      visit user_lists_path(user)
+
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
   end
 end
